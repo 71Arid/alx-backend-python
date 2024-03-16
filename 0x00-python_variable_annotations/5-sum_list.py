e6622ded97eb8b83df17835bde6cd258b1470d5c
71Arid <no232el@gmail.com>
2024-03-16 16:34:48 +0300

    sum_list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -11,6 +11,11 @@ of floats as arguments and returns their sum as a float
 def sum_list(input_list: List[float]) -> float:
     """
     for loop that finds sum of all float values
+    args:
+        input_list: A list of floats to be summed.
+
+    Returns:
+        The sum of the floats in the input_list, as a float.
     """
     s: float = 0
     for i in input_list:
