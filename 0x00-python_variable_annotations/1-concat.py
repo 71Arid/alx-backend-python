a95c7364c9127680413b21ace58b35b4da30b7b1
71Arid <no232el@gmail.com>
2024-03-16 16:00:17 +0300

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type annotated concat function that takes a string str1 and a string str2
+as arguments and retuns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+    """
+    return (str1 + str2)
