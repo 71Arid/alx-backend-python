1b6a2c885e0908d220c61d5a8c2e20b6f94575eb
71Arid <no232el@gmail.com>
2024-03-16 16:38:17 +0300

    sum list correction

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-from typing import List
 """
 This module uses List from typing library to enable type
 annotation
 type annotated function sum_list which takes a list input_list
 of floats as arguments and returns their sum as a float
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    for loop that finds sum of all float values
     args:
         input_list: A list of floats to be summed.
 
