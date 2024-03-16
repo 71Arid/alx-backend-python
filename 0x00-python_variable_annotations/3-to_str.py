4005b676f5688f7f7566869d333756388e5d4a2d
71Arid <no232el@gmail.com>
2024-03-16 16:09:18 +0300

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pycodestyle
+"""
+takes a float argument and returns a string representation
+of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns string from float n
+    """
+    return str(n)
