dce985e864b2c45dc6150ee7f11d9876721bf3fd
71Arid <no232el@gmail.com>
2024-03-16 17:14:24 +0300

    Callable value

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+type annotated function make_multiplier that takes a float multiplier
+as an argument and returns function that multiplies float
+by a multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    takes float and return a function that multiplies
+    multiplier by multiplier
+    """
+    def func(x: float) -> float:
+        return x * x
+    x: Callable[[float], float] = func
+    return x
