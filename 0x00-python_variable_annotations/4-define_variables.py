d83d8a7d1ca0e7f0b9072ff5b4ecda2067331e17
71Arid <no232el@gmail.com>
2024-03-16 16:16:39 +0300

    variables annotation

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Defining and annotating variables and their specific value
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
