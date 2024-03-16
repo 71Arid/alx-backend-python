9574dfea3bb23b689321a28b8603b0ca58ff8e4c
71Arid <no232el@gmail.com>
2024-03-16 17:32:05 +0300

    complex annotation

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,10 +2,10 @@
 """
 Annonating a given functiions parameter and return value
 """
-from typing import List, Iterator, Sequence, Tuple
+from typing import List, Iterable, Sequence, Tuple
 
 
-def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """a return value of type List[Tuple[Sequence, int]]
     """
     return [(i, len(i)) for i in lst]
