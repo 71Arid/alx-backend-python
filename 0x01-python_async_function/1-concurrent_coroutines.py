320f61ca29e16d5049a8d2d410505806ba97308b
71Arid <no232el@gmail.com>
2024-03-18 23:16:59 +0300

    concurrency

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+import wait_random from the previous python file that you've
+written and write an async routine wait_n takes 2 int arguments
+"""
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    srt_list: List[float] = []
+    for _ in range(n):
+        srt_list.append(wait_random(max_delay))
+    wait_n = await asyncio.gather(*srt_list)
+    return sorted(wait_n)
