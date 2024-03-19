0682eca630fe83467290b0dfea79443661d27ea4
71Arid <no232el@gmail.com>
2024-03-19 23:31:19 +0300

    measure runtime fixed pycodesttle error

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -13,7 +13,6 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-
 async def measure_runtime() -> float:
     """
     creates four async_comprehension instances
